5182f1f8c81cb584f86ca3bdf6c467597c3ab524
Harry Chan <taiharry108@gmail.com>
2019-05-06 22:38:18 -0700

    add get_model

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-from models import Model1 as Model
-from config import CAP_LEN, TRAIN_DIR, TEST_DIR, MODEL_FILENAME, MODEL_FILENAME, EPOCHS, LOG_INTERVAL, SEED, LR, BATCH_SIZE
-from helper_funcs import train, test, get_target_from_indices, get_preds_from_output, get_transformation
+# import models
+import models
+from config import CAP_LEN, TRAIN_DIR, TEST_DIR, MODEL_FILENAME, MODEL_FILENAME, EPOCHS, LOG_INTERVAL, SEED, LR, BATCH_SIZE, MODEL_NAME
+from utils import train, test, get_target_from_indices, get_preds_from_output, get_transformation
 from os.path import join
 
 from torchvision import datasets, transforms
 import torch
 import torch.optim as optim
 
+def get_model():
+    if not MODEL_NAME in models.__all__:
+        raise Except("MODEL_NAME does not exists")
+    else:
+        return models.__dict__[MODEL_NAME]
+
+
 def main():
     # Set the seed so results can be reproduced
     torch.manual_seed(SEED)
-    # Set torch to use CUDA
-    device = torch.device("cuda")
+    # Check if CUDA is available, use it if so
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
 
+    Model = get_model()
     model = Model().to(device)
     optimizer = optim.Adam(model.parameters(), lr=LR)
 
-    transforms = get_transformation()
+    transforms = get_transformation(model.img_width, model.img_height)
 
     # Set up image folder and loader for training and testing
     train_captcha_folder = datasets.ImageFolder(
@@ -28,14 +38,15 @@ def main():
     test_captcha_folder = datasets.ImageFolder(
         TEST_DIR, transform=transforms)
     test_loader = torch.utils.data.DataLoader(test_captcha_folder,
-                                               batch_size=1000,
-                                               shuffle=True,
-                                               num_workers=1)
+                                              batch_size=1000,
+                                              shuffle=True,
+                                              num_workers=1)
 
-    for epoch in range(1, epochs + 1):
+    for epoch in range(1, EPOCHS + 1):
         train(LOG_INTERVAL, model, device, train_loader, optimizer, epoch,
               get_target_from_indices, train_captcha_folder, MODEL_FILENAME)
-        test(model, device, test_loader, get_target_from_indices, test_captcha_folder)
+        test(model, device, test_loader,
+             get_target_from_indices, test_captcha_folder)
 
 
 if __name__ == "__main__":
