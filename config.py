c116f7e0f5108eec5f809cfb61da93d4ec077628
Harry Chan <taiharry108@gmail.com>
2019-05-06 22:38:02 -0700

    add TRAIN_DIR, TEST_DIR, MODEL_NAME

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # const for captcha generation
+from os.path import join
 DES_PATH = "gdrive/My Drive/projects/captcha_recognition"
 RESULT_FILE_NAME = 'captcha_img'
 
@@ -9,9 +10,13 @@ NO_TRAIN_CAP = 100000
 NO_TEST_CAP = 10000
 
 #config for model training
+TRAIN_DIR = join(DES_PATH, RESULT_FILE_NAME + '_train')
+TEST_DIR = join(DES_PATH, RESULT_FILE_NAME + '_test')
+
 MODEL_FILENAME = "model.pt"
 EPOCHS = 10
 LOG_INTERVAL = 10
 SEED = 1
 LR = 1e-3
 BATCH_SIZE = 128
+MODEL_NAME = "Model1"
