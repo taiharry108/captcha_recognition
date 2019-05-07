58ec2bf9924e4c0d08bbb42176e71f625c4d3607
Harry Chan <taiharry108@gmail.com>
2019-05-07 13:53:31 -0700

    add get_df

--- train_history.py
+++ train_history.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 
 class TrainHistory(object):
@@ -11,14 +12,46 @@ class TrainHistory(object):
         self.test_losses = [] if test_losses is None else test_losses
         self.train_acc = [] if train_acc is None else train_acc
         self.test_acc = [] if test_acc is None else test_acc
+        self.reset()
 
-    def add_train_history(self, loss, acc):
+    def reset(self):
+        self.epoch_train_loss = 0
+        self.epoch_train_correct = 0
+        self.epoch_train_n = 0
+        self.epoch_test_loss = 0
+        self.epoch_test_correct = 0
+        self.epoch_test_n = 0
+
+    def accmulate_train_history(self, loss, correct, n):
+        self.epoch_train_correct += correct
+        self.epoch_train_loss += loss * n
+        self.epoch_train_n += n
+
+    def accmulate_test_history(self, loss, correct, n):
+        self.epoch_test_correct += correct
+        self.epoch_test_loss += loss * n
+        self.epoch_test_n += n
+
+    def _add_train_history(self, loss, acc):
         self.train_losses.append(loss)
         self.train_acc.append(acc)
 
-    def add_test_history(self, loss, acc):
+    def _add_test_history(self, loss, acc):
         self.test_losses.append(loss)
         self.test_acc.append(acc)
+    
+    def epoch_finish(self):
+        train_loss = self.epoch_train_loss / self.epoch_train_n
+        train_acc = self.epoch_train_correct / self.epoch_train_n
+
+        test_loss = self.epoch_test_loss / self.epoch_test_n
+        test_acc = self.epoch_test_correct / self.epoch_test_n
+        
+        self._add_train_history(train_loss, train_acc)
+        self._add_test_history(test_loss, test_acc)
+
+        self.reset()
+
 
     def save_history(self):
         history = {
@@ -41,3 +74,12 @@ class TrainHistory(object):
                    train_acc=history_dict['train_acc'],
                    test_acc=history_dict['test_acc'],
                    )
+    
+    def get_df(self):
+        return pd.DataFrame(
+            {
+                "train loss": self.train_losses,
+                "train acc": self.train_acc,
+                "test loss": self.test_losses,
+                "test acc": self.test_acc
+            })
